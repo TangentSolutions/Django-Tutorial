1e9319b5df4d29bbba3cbfc42b9b5e674f575d0c
Ian Roberts <ian@tangentsolutions.co.za>
2016-07-14 23:55:34 +0200

    Using templates

--- hr/urls.py
+++ hr/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^datetime/$', views.current_datetime, name='current_datetime'),
+	# ex: /hr/5/
+    url(r'^(?P<tangeneer_id>[0-9]+)/$', views.detail, name='detail'),
 ]
--- hr/views.py
+++ hr/views.py
@@ -1,11 +1,19 @@
 from django.http import HttpResponse
 import datetime
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello Tangeneer!")
+	return HttpResponse("Hello Tangeneers!")
 
 def current_datetime(request):
-    now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
-    return HttpResponse(html)
+	now = datetime.datetime.now()
+	html = "<html><body>It is now %s.</body></html>" % now
+	return HttpResponse(html)
 
+def detail(request, tangeneer_id):
+
+	template = "hr/detail.html"
+
+	context = {'tangeneer': 'Someones details'}
+
+	return render(request, template, context)
