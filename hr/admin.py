cc2f49c3eab1f3df0a99bf0535b2cca19862dd25
Ian Roberts <ian@tangentsolutions.co.za>
2016-07-15 14:50:30 +0200

    Admin site

--- hr/admin.py
+++ hr/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+class TaskAdmin(admin.ModelAdmin):
+	list_display = ('title', 'due_date')
+
+class SkillAdmin(admin.ModelAdmin):
+	pass
+
+class JobAdmin(admin.ModelAdmin):
+	list_display = ('title', 'category', 'is_active')
+	list_filter = ('category',)
+	ordering = ('-created',)
+
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Skill, SkillAdmin)
+admin.site.register(Job, JobAdmin)
