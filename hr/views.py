035f11e8e08617bd2a1cce84ea402818173b287d
Ian Roberts <ian@tangentsolutions.co.za>
2016-07-23 12:25:22 +0200

    Add First Views for Authentication

--- hr/views.py
+++ hr/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 import datetime
 from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from serializers import *
 
 def index(request):
 	return HttpResponse("Hello Tangeneers!")
@@ -17,3 +20,18 @@ def detail(request, tangeneer_id):
 	context = {'tangeneer': 'Someones details'}
 
 	return render(request, template, context)
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
--- tangeneer/settings.py
+++ tangeneer/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-    'hr'
+    'hr',
+
+    ## 3rd party
+    'rest_framework',
+    'rest_framework_swagger',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -72,6 +76,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'tangeneer.wsgi.application'
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    'PAGE_SIZE': 10
+}
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
