059a3360621931153e068b9254d5da66d8481b41
Christiaan Vermeulen <chris@cmsoft.co.za>
2016-08-17 10:28:38 +0200

    add all api endpoints and general enhancements

--- hr/views.py
+++ hr/views.py
@@ -1,19 +1,44 @@
 from django.http import HttpResponse
 import datetime
 from django.shortcuts import render
+from rest_framework import viewsets
+from hr.serializers import *
 
-def index(request):
-	return HttpResponse("Hello Tangeneers!")
+class JobViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows tasks to be viewed or edited.
+    """
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+ 
 
-def current_datetime(request):
-	now = datetime.datetime.now()
-	html = "<html><body>It is now %s.</body></html>" % now
-	return HttpResponse(html)
+class TaskViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows tasks to be viewed or edited.
+    """
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
 
-def detail(request, tangeneer_id):
 
-	template = "hr/detail.html"
+class SkillViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows skills to be viewed or edited.
+    """
+    queryset = Skill.objects.all()
+    serializer_class = SkillSerializer
 
-	context = {'tangeneer': 'Someones details'}
 
-	return render(request, template, context)
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
--- tangeneer/urls.py
+++ tangeneer/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-	url(r'^hr/', include('hr.urls')),
-	url(r'^admin/', admin.site.urls),
+    url(r'^api/', include('hr.urls')),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^admin/', admin.site.urls)
 ]
